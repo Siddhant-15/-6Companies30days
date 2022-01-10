e20c783351227fcb6c81254ad8f8604d71ee84a4
Siddhant-15 <63910007+Siddhant-15@users.noreply.github.com>
2022-01-10 21:58:27 +0530

    Create Nuts and Bolts Problem .cpp

---
+++ Amazon/Nuts and Bolts Problem .cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void matchPairs(char nuts[], char bolts[], int n)
+    {
+        // code here
+        set<int> nuts1;
+        set<int> bolts1;
+
+        for (int i = 0; i < n; i++)
+        {
+            nuts1.insert(nuts[i]);
+            bolts1.insert(bolts[i]);
+        }
+        auto itr = nuts1.begin();
+        for (int i = 0; i < n; i++)
+        {
+            if (nuts1.find(bolts[i]) != nuts1.end() && bolts1.find(nuts[i]) != bolts1.end())
+            {
+                nuts[i] = *itr;
+                bolts[i] = *itr;
+            }
+            itr++;
+        }
+    }
+};
